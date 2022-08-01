3e21662641b1a6f4de39ff1aadae2a39f3667db2
Jeis <sirjeis15@gmail.com>
2022-08-01 19:38:40 -0400

    Inline JS

---
+++ script.js
@@ -0,0 +1,17 @@
+var clicker = {
+    Experience:0,
+    Level:1
+   };
+    function thing_clicked(thing){
+        if(clicker.Experience >= 100){
+            clicker['Level']+=1;
+            clicker['Experience']=0;
+        }
+        clicker['Experience']+=1;
+    }
+    function updatecount(){
+        setInterval(() => {
+            document.querySelector("#exp").innerHTML = "You have "+clicker.Experience+" Experience"
+            document.querySelector("#lvl").innerHTML = "You are Level "+clicker.Level
+        },50);
+    }
