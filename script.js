014bd5076d90e1c10275e922848697f80a496e85
Jeis <sirjeis15@gmail.com>
2022-08-01 19:50:15 -0400

    Version 0.2

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var clicker = {
     Experience:0,
-    Level:1
+    Level:1,
+    Gold:0
+    
    };
     function thing_clicked(thing){
         if(clicker.Experience >= 100){
@@ -8,10 +10,12 @@ var clicker = {
             clicker['Experience']=0;
         }
         clicker['Experience']+=1;
+        clicker['Gold']+=1
     }
     function updatecount(){
         setInterval(() => {
             document.querySelector("#exp").innerHTML = "You have "+clicker.Experience+" Experience"
             document.querySelector("#lvl").innerHTML = "You are Level "+clicker.Level
+            document.querySelector("#money").innerHTML = "Gold: "+clicker.Gold
         },50);
     }
