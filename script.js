f17e44f54f658baf5539734f3726fa722238312d
Jeis <sirjeis15@gmail.com>
2022-08-02 16:47:30 -0400

    class update

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ var clicker = {
     swordcost: 500,
     WeaponValue: 0,
     dmg: 1,
+    class: "None",
    };
 
     function thing_clicked(){
@@ -18,6 +19,43 @@ var clicker = {
 
     function slay_Goblin(){
         clicker.GoblinHP-=clicker.dmg;
+    }
+
+    function cheat(){
+        document.querySelector(".devgold").style.display = 'inline';
+        document.querySelector(".devlevel").style.display = 'inline';
+    }
+
+    function addGold(){
+        clicker.Gold += 100;
+    }
+
+    function addLevel(){
+        clicker.Experience += 100000000;
+    }
+
+    function choose_Warrior(){
+        clicker.class = "Myrmidon"
+        document.querySelector(".select").style.display = 'none';
+        document.querySelector(".main").style.display = 'inline';
+        document.querySelector(".inventory").style.display = 'inline';
+        document.querySelector(".warriorclass").style.display = 'inline';
+    }
+
+    function choose_Assassin(){
+        clicker.class = "Assassin"
+        document.querySelector(".select").style.display = 'none';
+        document.querySelector(".main").style.display = 'inline';
+        document.querySelector(".inventory").style.display = 'inline';
+        document.querySelector(".assassinclass").style.display = 'inline';
+    }
+
+    function choose_Warlock(){
+        clicker.class = "Warlock"
+        document.querySelector(".select").style.display = 'none';
+        document.querySelector(".main").style.display = 'inline';
+        document.querySelector(".inventory").style.display = 'inline';
+        document.querySelector(".mageclass").style.display = 'inline';
     }
 
 
@@ -36,20 +74,61 @@ var clicker = {
         if(clicker.Gold >= 100){
             clicker['Gold']-=clicker['daggercost'];
             clicker['Weapon'] = "Dagger"
-            clicker['dmg'] +=1;
+            clicker['dmg'] +=2;
             clicker['WeaponValue'] = 1;
             document.querySelector(".dagger").style.display = 'none';
-            document.querySelector(".sword").style.display = 'inline';
+            document.querySelector(".sharpdagger").style.display = 'inline';
         }
     }
 
+    function buy_Sharp(){
+        if(clicker.Gold >= 100){
+            clicker['Gold']-=500;
+            clicker['Weapon'] = "Sharpened Dagger"
+            clicker['dmg'] -=2;
+            clicker['dmg'] +=5;
+            document.querySelector(".sharpdagger").style.display = 'none';
+        }
+    }
+
+   function buy_woodSword(){
+    if(clicker.Gold >= 100){
+        clicker['Gold']-=100;
+        clicker['dmg'] += 2;
+        clicker['Weapon'] = "Wooden Sword";
+        document.querySelector(".woodensword").style.display = 'none';
+        document.querySelector(".sword").style.display = 'inline';
+    }
+   }
+    
     function buy_Sword(){
         if(clicker.Gold >= 500){
             clicker['Gold']-=clicker['swordcost'];
-            clicker['dmg'] += 3;
+            clicker['dmg'] -= 2;
+            clicker['dmg'] += 5;
             clicker['Weapon'] = "Sword"
             clicker['WeaponValue'] = 2;
             document.querySelector(".sword").style.display = 'none';
+        }
+    }
+
+    function buy_Orb(){
+        if(clicker.Gold >= 100){
+            clicker['Gold']-=100;
+            clicker['dmg'] += 2;
+            clicker['Weapon'] = "Orb";
+            document.querySelector(".orb").style.display = 'none';
+            document.querySelector(".woodstaff").style.display = 'inline';
+        }
+    }
+
+    function buy_woodStaff(){
+        if(clicker.Gold >= 100){
+            clicker['Gold']-=500;
+            clicker['Weapon'] = "Wooden Staff"
+            clicker['dmg'] -=2;
+            clicker['dmg'] +=5;
+            document.querySelector(".woodstaff").style.display = 'none';
         }
     }
 
@@ -62,6 +141,7 @@ var clicker = {
             document.querySelector("#Goblin").innerHTML = "Slay a Goblin <br>HP:"+clicker.GoblinHP+"/50"
             document.querySelector("#weapon").innerHTML = "Weapon: "+clicker.Weapon
             document.querySelector("#count").innerHTML = "Damage: "+clicker.dmg
+            document.querySelector("#role").innerHTML = "Class: "+clicker.class
             if(clicker.SlimeHP <= 0){
                 clicker['Experience']+=1;
                 clicker['Gold']+=1;
