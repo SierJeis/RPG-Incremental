0f75b99cf2fabb038a2cf7956c06500d279118bd
Jeis <sirjeis15@gmail.com>
2022-08-01 20:15:12 -0400

    Version 0.25 The Slime Update

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var clicker = {
     Experience:0,
     Level:1,
-    Gold:0
+    Gold:0,
+    SlimeHP:10,
     
    };
     function thing_clicked(thing){
@@ -9,13 +10,18 @@ var clicker = {
             clicker['Level']+=1;
             clicker['Experience']=0;
         }
-        clicker['Experience']+=1;
-        clicker['Gold']+=1
+        if(clicker.SlimeHP <= 0){
+            clicker['Experience']+=1;
+            clicker['Gold']+=1;
+            clicker['SlimeHP'] = 11
+        }
+        clicker.SlimeHP-=1
     }
     function updatecount(){
         setInterval(() => {
             document.querySelector("#exp").innerHTML = "You have "+clicker.Experience+" Experience"
             document.querySelector("#lvl").innerHTML = "You are Level "+clicker.Level
             document.querySelector("#money").innerHTML = "Gold: "+clicker.Gold
+            document.querySelector("#slime").innerHTML = "Slay a slime <br>HP: "+clicker.SlimeHP
         },50);
     }
